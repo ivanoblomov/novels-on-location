25e481e608d8377ea6d03cb58a068fb868e4d935
Roderick Monje <rod@seologic.com>
2011-05-25 10:26:54 -0500

    fixed nasty typos in to_google_lat_lng calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def to_google_lat_lng( lat_lng_array )
-    "new google.maps.LatLng(#{@location.lat_lng * ', '})"
+    "new google.maps.LatLng(#{lat_lng_array * ', '})"
   end
 end
