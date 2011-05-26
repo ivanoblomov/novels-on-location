78751836c5325482956b8bb4c6106eb09438fe98
Roderick Monje <rod@seologic.com>
2011-05-25 21:52:47 -0500

    page speed: Remove query strings from static resources

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['RAILS_ASSET_ID'] = ''
+
 # Initialize the rails application
 NovelsOnLocation::Application.initialize!
 
