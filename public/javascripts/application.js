1065641075cc92c11badd472883bce1784225118
Roderick Monje <rod@seologic.com>
2011-05-28 21:08:20 -0500

    removed lat/lng display

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,76 +1,3 @@
-// Adapted from http://gmaps-samples-v3.googlecode.com/svn/trunk/latlng-to-coord-control/latlng-to-coord-control.html
-
-/**
- * LatLngControl class displays the LatLng and pixel coordinates
- * underneath the mouse within a container anchored to it.
- * @param {google.maps.Map} map Map to add custom control to.
- */
-function LatLngControl(map) {
-  /**
-   * Offset the control container from the mouse by this amount.
-   */
-  this.ANCHOR_OFFSET_ = new google.maps.Point(8, 8);
-
-  /**
-   * Pointer to the HTML container.
-   */
-  this.node_ = this.createHtmlNode_();
-
-  // Add control to the map. Position is irrelevant.
-  map.controls[google.maps.ControlPosition.TOP].push(this.node_);
-
-  // Bind this OverlayView to the map so we can access MapCanvasProjection
-  // to convert LatLng to Point coordinates.
-  this.setMap(map);
-
-  // Register an MVC property to indicate whether this custom control
-  // is visible or hidden. Initially hide control until mouse is over map.
-  this.set('visible', false);
-}
-
-// Extend OverlayView so we can access MapCanvasProjection.
-LatLngControl.prototype = new google.maps.OverlayView();
-LatLngControl.prototype.draw = function() {};
-
-/**
- * @private
- * Helper function creates the HTML node which is the control container.
- * @return {HTMLDivElement}
- */
-LatLngControl.prototype.createHtmlNode_ = function() {
-  var divNode = document.createElement('div');
-  divNode.id = 'latlng-control';
-  divNode.index = 100;
-  return divNode;
-};
-
-/**
- * MVC property's state change handler function to show/hide the
- * control container.
- */
-LatLngControl.prototype.visible_changed = function() {
-  this.node_.style.display = this.get('visible') ? '' : 'none';
-};
-
-/**
- * Specified LatLng value is used to calculate pixel coordinates and
- * update the control display. Container is also repositioned.
- * @param {google.maps.LatLng} latLng Position to display
- */
-LatLngControl.prototype.updatePosition = function(latLng) {
-  var projection = this.getProjection();
-  var point = projection.fromLatLngToContainerPixel(latLng);
-
-  // Update control position to be anchored next to mouse position.
-  this.node_.style.left = point.x + this.ANCHOR_OFFSET_.x + 'px';
-  this.node_.style.top = point.y + this.ANCHOR_OFFSET_.y + 'px';
-
-  // Update control to display latlng and coordinates.
-  this.node_.innerHTML = [
-    latLng.toUrlValue(4)
-  ].join('');
-};
-
 // Adapted from http://code.google.com/apis/maps/documentation/javascript/examples/icon-complex.html
 
 var bookPrompt = 'Find a mapped book';
@@ -101,19 +28,7 @@ function initializeMap() {
   $('book-input').value = bookPrompt;
   $('place-input').value = placePrompt;
 
-  // Create new control to display latlng and coordinates under mouse.
-  var latLngControl = new LatLngControl(map);
-
   // Register event listeners
-  google.maps.event.addListener(map, 'mouseover', function(e) {
-    latLngControl.set('visible', true);
-  });
-  google.maps.event.addListener(map, 'mouseout', function(e) {
-    latLngControl.set('visible', false);
-  });
-  google.maps.event.addListener(map, 'mousemove', function(e) {
-    latLngControl.updatePosition(e.latLng);
-  });
   google.maps.event.addListener(map, 'click', function(e) {
     promptForTitle(e.latLng);
   });
