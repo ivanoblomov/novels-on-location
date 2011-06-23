7e3fbd09e78a703bb56cf8fc9fda178ced9d41b2
Roderick Monje <rod@seologic.com>
2011-06-23 17:55:15 -0500

    page speed: Leverage browser caching, Specify a Vary: Accept-Encoding header

---
+++ config/initializers/heroku.rb
@@ -0,0 +1,11 @@
+module Heroku
+  class StaticAssetsMiddleware
+    def cache_static_asset(reply)
+      return reply unless can_cache?(reply)
+      status, headers, response = reply
+      headers['Cache-Control'] = 'public, max-age=31556926' # 1 year
+      headers['Vary'] = 'Accept-Encoding'
+      build_new_reply(status, headers, response)
+    end
+  end
+end
