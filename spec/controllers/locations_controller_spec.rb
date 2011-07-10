cf3505b9c7e466b84435a479b20dac7c8eaca747
Roderick Monje <rod@seologic.com>
2011-07-10 09:40:22 -0500

    spec'd C (one failure exists)

---
+++ spec/controllers/locations_controller_spec.rb
@@ -0,0 +1,41 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe LocationsController do
+  before :each do
+    @location = Location.new :id => 1, :lat_lng => ['1', '2'], :user_id => 1
+  end
+
+  it 'gets index action' do
+    Location.expects(:all).returns([@location]).at_least_once
+    get :index
+    assert assigns(:locations)
+    response.should render_template('layouts/application')
+  end
+
+  it 'gets show action' do
+    controller.stubs(:render)
+    Location.expects(:new)
+    get :show, :id => 1
+    asserts assigns(:location).instance_of(Location)
+  end
+
+  it 'posts create action' do
+    controller.stubs(:render)
+    Location.expects(:create)
+    post :create, :location => {:title => nil}
+  end
+
+  it 'puts update action' do
+    controller.stubs(:render)
+    Location.expects(:find).returns(@location)
+    @location.expects(:update_attributes)
+    put :update, :id => 1, :location => {:title => nil}
+  end
+
+  it 'deletes destroy action and redirects' do
+    controller.stubs(:render)
+    Location.expects(:find).returns(@location)
+    @location.expects(:destroy)
+    delete :destroy, :id => 1
+  end
+end
