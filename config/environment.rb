59d037f95cdb211cdaf68e2157c25749591dd499
Roderick Monje <ioblomov@comcast.net>
2011-10-01 17:44:08 -0500

    added mongohq config

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ NovelsOnLocation::Application.configure do
   config.amazon_access_key_id = 'AKIAINFSZKSQ4ZTKFDZA'
   config.amazon_secret_access_key = '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'
   config.facebook_app_id = '216729245030640'
-  config.production_mongohq_db = 'app540417'
-  config.production_mongohq_password = 'uqdb1uz5eqdrjmyrfi9okc'
-  config.production_mongohq_port = '27107'
+  config.production_mongohq_db = 'app1303578'
+  config.production_mongohq_password = 'f4c76d2cb1e2774d7a6f46a154aa88e8'
+  config.production_mongohq_port = '10044'
 end
