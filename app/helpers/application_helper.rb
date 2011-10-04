92874f4c98b06a024b92170940cc93d2675d4c77
Roderick Monje <ioblomov@comcast.net>
2011-10-03 21:20:14 -0500

    styled fb-page icon: CSSs are mutually exclusive (instead of inherited); extracted user-agent helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def filter_param(value)
     "'#{value.blank? ? nil : j(value)}'"
   end
+
+  def ie?
+    !! (request.user_agent =~ /msie/i)
+  end
+
+  def ios?
+    !! (request.user_agent =~ /ipad|iphone/i)
+  end
+
+  def safari_pc?
+    !! (request.user_agent =~ /safari/i) && ! ios?
+  end
 end
