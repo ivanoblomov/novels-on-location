2e42f33259976d9d1b1433e6977552348f8fd3f1
Roderick Monje <ioblomov@comcast.net>
2011-10-19 23:19:37 -0500

    removed extraneous files, lines

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 NovelsOnLocation::Application.config.secret_token = '7b0733f7f46211e3345bb4144cb90acf15f5bebeaeca46ce573d603394f8c5ab3feeca18e52ad6fdb5bcd21c87e528dc39eba3f0b51e532b050aa8810322294c'
