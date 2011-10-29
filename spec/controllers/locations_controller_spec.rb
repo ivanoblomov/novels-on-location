4cfc2ba15a772f0fa5b4173630bd9fc6514390cd
Roderick Monje <ioblomov@comcast.net>
2011-10-29 15:57:53 -0500

    fixed spec-helper requires

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe LocationsController do
   before :each do
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe Location do
   it 'sets its latitude and longitude' do
