13a277af27b34342a05a4eb7384d4c5cb2bb9549
Roderick Monje <ioblomov@comcast.net>
2012-02-04 17:19:48 -0600

    updated location's meta-description; removed extra spaces in config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ASSET_ID'] = ''
 NovelsOnLocation::Application.configure do
   config.cache_classes = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
   config.serve_static_assets = false
