540df2cf7e020d2fe981cbcd5e4438d43cbf8fc1
Roderick Monje <ioblomov@comcast.net>
2012-02-25 18:35:23 -0600

    updated admin email

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../application', __FILE__)
 NovelsOnLocation::Application.initialize!
 NovelsOnLocation::Application.configure do
-  config.admin_email = 'admin@novelsonlocation.com'
+  config.admin_email = 'ed@novelsonlocation.com'
   config.amazon_access_key_id = 'AKIAINFSZKSQ4ZTKFDZA'
   config.amazon_secret_access_key = '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'
   config.facebook_app_id = '216729245030640'
