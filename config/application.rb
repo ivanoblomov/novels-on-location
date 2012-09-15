ce92124906cc8e2cae23da9c40ba2cb18da4f927
Roderick Monje <ioblomov@comcast.net>
2012-09-15 07:23:32 -0500

    redirected google "not found" errors

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,13 @@ module NovelsOnLocation
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+      r301 '/locations/fiesta-arrow-classic-2','/locations/fiesta-arrow-classic-1'
+      r301 '/locations/ice-drift-1','/locations/ice-drift'
+      r301 '/locations/la-famiglia-moskat-1','/locations/la-famiglia-moskat'
+      r301 '/locations/la-famiglia-moskat-2','/locations/la-famiglia-moskat'
+      r301 '/locations/plastiki-across-the-pacific-on-plastic-an-adventure-to-save-our-oceans-1','/locations/plastiki-across-the-pacific-on-plastic-an-adventure-to-save-our-oceans'
       r301 '/locations/shermans-march-the-first-full-length...', '/locations/shermans-march-the-first-full-length-narrative-of-general-william-t-shermans-devastating-march-through-georgia-and-the-carolinas'
+      r301 '/locations/the-adventures-of-tom-sawyer-1','/locations/the-adventures-of-tom-sawyer'
     end
   end
 end
