6878ebdd64d664175f7ebd451f81eda077fbdaa9
Roderick Monje <rod@foveacentral.com>
2015-12-27 17:48:06 -0500

    sys: fix assets, mongoid config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
-Rails.application.config.assets.precompile += %w(safari.css google_analytics.js)
+Rails.application.config.assets.precompile += %w(
+  safari.css google_analytics.js 1140.css admin.css
+)
