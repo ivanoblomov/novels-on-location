acf994a9fb4c0e87c3036c616da9bd136bbd7b34
Roderick Monje <rod@foveacentral.com>
2016-01-23 02:04:08 -0500

    fix: Twitter oauth_token warnings

--- app/models/location.rb
+++ app/models/location.rb
@@ -78,8 +78,8 @@ class Location
   @twitter_client = Twitter::Streaming::Client.new do |config|
     config.consumer_key = 'Ms7Cl2g9eM0sZKRl8YA34Q'
     config.consumer_secret = 'gQlXn8I9TtLcNSepF5D59DBkSI0Wv9pYl1465iAc4'
-    config.oauth_token = '490732052-SDJHy8huJ9J4Ic7aNIiR4T4XZiBxbMQ2eW2Qi2oz'
-    config.oauth_token_secret = 'N8B6ZPg0gxtqLOeEI7LVKbHCdXvZpHJqpRmmPNSWk'
+    config.access_token = '490732052-SDJHy8huJ9J4Ic7aNIiR4T4XZiBxbMQ2eW2Qi2oz'
+    config.access_token_secret = 'N8B6ZPg0gxtqLOeEI7LVKbHCdXvZpHJqpRmmPNSWk'
   end
 
   def self.book_count
