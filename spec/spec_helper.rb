5c65888cc75dbff548affcc0d4443a8e8a3948fb
Roderick Monje <rod@foveacentral.com>
2024-11-12 13:54:57 -0800

    fix: bizarre ActiveRecord errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ SimpleCov.start
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+Object.send(:remove_const, :ActiveRecord) # hack to avoid mysterious ActiveRecord::ConnectionNotEstablished
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
