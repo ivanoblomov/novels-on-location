8daadba6b73f3e052302dd8ec8e15abfc168d5e1
Roderick Monje <rod@foveacentral.com>
2024-11-14 23:28:34 -0800

    conf

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('application', __dir__)
 NovelsOnLocation::Application.initialize!
 NovelsOnLocation::Application.configure do
   config.admin_email = 'ed@novelsonlocation.com'
-  config.amazon_access_key_id = ENV.fetch('AMAZON_ACCESS_KEY_ID')
-  config.amazon_secret_access_key = ENV.fetch('AMAZON_SECRET_ACCESS_KEY')
-  config.facebook_app_id = ENV.fetch('FACEBOOK_APP_ID')
+  config.amazon_access_key_id = ENV.fetch('AMAZON_ACCESS_KEY_ID', nil)
+  config.amazon_secret_access_key = ENV.fetch('AMAZON_SECRET_ACCESS_KEY', nil)
+  config.facebook_app_id = ENV.fetch('FACEBOOK_APP_ID', nil)
 end
