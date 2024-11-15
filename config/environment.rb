85b2264e2b3102f638a2a42f8933d19e98e417cb
Roderick Monje <ivanoblomov@users.noreply.github.com>
2024-11-14 23:47:07 -0800

    conf (#20)
    # Closes: n/a
    
    ## Goal
    Consolidate and secure credentials.
    
    ## Approach
    Initialize credentials consistently and remove them from source control.
    1. Rename `MAPS_KEY` to `GOOGLE_MAPS_API_KEY` for consistency with
    `GoogleMapsGeocoder`.
    2. Add missing keys to `_application.yml` (which will set `ENV`).
    3. Reference credentials from `ENV`.
    4. Update `config.time_zone`.
    
    Signed-off-by: Roderick Monje <rod@foveacentral.com>

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('application', __dir__)
 NovelsOnLocation::Application.initialize!
 NovelsOnLocation::Application.configure do
   config.admin_email = 'ed@novelsonlocation.com'
-  config.amazon_access_key_id = 'AKIAINFSZKSQ4ZTKFDZA'
-  config.amazon_secret_access_key = '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'
-  config.facebook_app_id = '216729245030640'
+  config.amazon_access_key_id = ENV.fetch('AMAZON_ACCESS_KEY_ID', nil)
+  config.amazon_secret_access_key = ENV.fetch('AMAZON_SECRET_ACCESS_KEY', nil)
+  config.facebook_app_id = ENV.fetch('FACEBOOK_APP_ID', nil)
 end
