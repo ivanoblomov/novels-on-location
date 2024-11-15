723806fa853ceab48ce2469f4d3f03d7bb3fe38a
Roderick Monje <rod@foveacentral.com>
2024-11-14 23:26:31 -0800

    conf

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-NovelsOnLocation::Application.config.secret_key_base = ENV.fetch('SECRET_KEY_BASE')
+NovelsOnLocation::Application.config.secret_key_base = ENV.fetch('SECRET_KEY_BASE', nil)
