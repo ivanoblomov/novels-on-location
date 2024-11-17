d8c084e3e2f9109595acb3b81c46a86edca1b91c
Roderick Monje <rod@foveacentral.com>
2024-11-16 22:55:58 -0800

    conf

--- features/support/env.rb
+++ features/support/env.rb
@@ -51,3 +51,5 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :deletion
+# disable due to https://github.com/DatabaseCleaner/database_cleaner-mongoid/issues/16
+Cucumber::Rails::Database.autorun_database_cleaner = false
