54fc0e1e0045974fd063ecbac3a985fceb6eb3d9
Roderick Monje <rod@foveacentral.com>
2024-11-17 15:27:20 -0800

    conf

---
+++ features/support/capybara.rb
@@ -0,0 +1,18 @@
+require 'capybara/rspec'
+require 'selenium/webdriver'
+
+Capybara.configure do |config|
+  config.default_max_wait_time = 5 # seconds
+  config.default_driver = :selenium
+  config.javascript_driver = :selenium
+  config.app_host = 'localhost:3000'
+end
+
+Capybara.register_driver :selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  Capybara::Selenium::Driver.new(app,
+    browser: :chrome,
+    options: options
+  )
+end
