848c6b9befa3897dbf1a9f7360be224173e575af
Roderick Monje <rod@foveacentral.com>
2024-11-18 14:14:45 -0800

    conf

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,7 +73,11 @@ end
 Capybara.always_include_port = true
 Capybara.register_driver :selenium do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
   options.add_argument('--disable-dev-shm-usage') # overcome limited resource problems
+  options.add_argument('--window-size=1366,720')
 
   Capybara::Selenium::Driver.new(app,
                                  browser: :chrome,
