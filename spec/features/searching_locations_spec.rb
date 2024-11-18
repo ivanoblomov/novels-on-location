6ebee4d5329949947a3414b48a440fc935de31c5
Roderick Monje <rod@foveacentral.com>
2024-11-17 23:24:44 -0800

    spec

---
+++ spec/features/searching_locations_spec.rb
@@ -0,0 +1,7 @@
+RSpec.feature 'Searching for Locations', type: :feature do
+  scenario 'User searches for a Location' do
+    visit root_path
+    fill_in '#book-input', with: Location.first.title
+    # should hide all other pins
+  end
+end
