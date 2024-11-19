0b1c59c78a9ca3327d84e3052eb676c68435ae44
Roderick Monje <rod@foveacentral.com>
2024-11-19 13:26:19 -0800

    spec: searching by author, tag

--- spec/features/searching_locations_spec.rb
+++ spec/features/searching_locations_spec.rb
@@ -7,21 +7,32 @@ RSpec.describe 'Novels: On Location', js: !ENV['GITHUB_ACTIONS'], type: :system 
     pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
   else
     context 'when multiple Locations exist' do
+      subject(:visible_pins) { evaluate_script script }
+
+      let(:script) do
+        'Object.values(nOL.pins).filter((pin) => pin.map).length;' # count visible pins (where map is defined)
+      end
       let(:sun_also_rises) do
         Location.destroy_all
-        Location.create title: 'From Hell',
-                        lat_lng: ['51.519326', '-0.074316']
-        Location.create title: 'The Sun Also Rises',
+        Location.create author: 'Alan Moore',
+                        lat_lng: ['51.519326', '-0.074316'],
+                        tags: 'The Ten Bells, Spitalfields',
+                        title: 'From Hell'
+        Location.create author: 'Ernest Hemingway',
+                        title: 'The Sun Also Rises',
                         lat_lng: ['42.817422', '-1.64325']
       end
 
-      context 'when User searches for a specific Location' do
-        subject(:visible_pins) do
-          # count visible pins (where map is defined)
-          script = 'Object.values(nOL.pins).filter((pin) => pin.map).length;'
-          evaluate_script script
+      context 'when User searches for an author' do
+        before do
+          visit root_path
+          fill_in 'book-input', with: sun_also_rises.author
         end
 
+        it('hides other Locations') { expect(visible_pins).to eq 1 }
+      end
+
+      context 'when User searches for a title' do
         before do
           visit root_path
           fill_in 'book-input', with: sun_also_rises.title
@@ -30,20 +41,14 @@ RSpec.describe 'Novels: On Location', js: !ENV['GITHUB_ACTIONS'], type: :system 
         it('hides other Locations') { expect(visible_pins).to eq 1 }
       end
 
-#       Scenario: Visitor searches for an author
-#         Given multiple pins
-#         When I search for an author matching one of their books
-#         Then it should hide all the other pins
-#
-#       Scenario: Visitor searches for a title
-#         Given multiple pins
-#         When I search for a title matching one of their books
-#         Then it should hide all the other pins
-#
-#       Scenario: Visitor searches for a tag
-#         Given multiple pins
-#         When I search for a tag matching one of them
-#         Then it should hide all the other pins
+      context 'when User searches for a tag' do
+        before do
+          visit root_path
+          fill_in 'book-input', with: 'ten bells'
+        end
+
+        it('hides other Locations') { expect(visible_pins).to eq 1 }
+      end
     end
   end
 end
