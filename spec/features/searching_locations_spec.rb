8a4b23a3bc39872b0ff16bc738233f231fe411a1
Roderick Monje <ivanoblomov@users.noreply.github.com>
2024-11-19 12:50:24 -0800

    Style feature specs (#30)
    # Re: #27
    
    ## Goal
    Style features to be more idiomatic.
    
    ## Approach
    1. Extract semantic blocks:
      a. `context`
      b. `let`
      c. `subject`
      d. `before`
      e. `it`
    
    Signed-off-by: Roderick Monje <rod@foveacentral.com>

--- spec/features/searching_locations_spec.rb
+++ spec/features/searching_locations_spec.rb
@@ -2,25 +2,33 @@
 
 require 'rails_helper'
 
-# rubocop:disable RSpec/ExampleLength
-RSpec.describe 'Searching for Locations', js: !ENV['GITHUB_ACTIONS'], type: :system do
-  scenario 'User searches for a Location' do
-    if ENV['GITHUB_ACTIONS']
-      pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
-      raise
-    else
-      Location.destroy_all
-      Location.create title: 'The Sun Also Rises',
-                      lat_lng: ['42.817422', '-1.64325']
-      Location.create title: 'From Hell',
-                      lat_lng: ['51.519326', '-0.074316']
-      visit root_path
-      fill_in 'book-input', with: Location.first.title
-      # count visible pins (where map is defined)
-      script = 'Object.values(nOL.pins).filter((pin) => pin.map).length;'
-      pin_count = evaluate_script script
-      expect(pin_count).to eq 1
+RSpec.describe 'Novels: On Location', js: !ENV['GITHUB_ACTIONS'], type: :system do
+  if ENV['GITHUB_ACTIONS']
+    pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
+  else
+    context 'when multiple Locations exist' do
+      let(:sun_also_rises) do
+        Location.destroy_all
+        Location.create title: 'From Hell',
+                        lat_lng: ['51.519326', '-0.074316']
+        Location.create title: 'The Sun Also Rises',
+                        lat_lng: ['42.817422', '-1.64325']
+      end
+
+      context 'when User searches for a specific Location' do
+        subject(:visible_pins) do
+          # count visible pins (where map is defined)
+          script = 'Object.values(nOL.pins).filter((pin) => pin.map).length;'
+          evaluate_script script
+        end
+
+        before do
+          visit root_path
+          fill_in 'book-input', with: sun_also_rises.title
+        end
+
+        it('hides other Locations') { expect(visible_pins).to eq 1 }
+      end
     end
   end
 end
-# rubocop:enable RSpec/ExampleLength
