7cf2cfa1c4f03599d2002df4c9605e1c48d5f89f
Roderick Monje <ivanoblomov@users.noreply.github.com>
2024-11-19 15:19:34 -0800

    spec: browsing Locations (#31)
    # Re: #27
    
    ## Goal
    Spec remaining features.
    
    ## Approach
    1. Copy over Cucumber features from #27.
    2. Convert to RSpec.
    
    Signed-off-by: Roderick Monje <rod@foveacentral.com>

---
+++ spec/features/adding_locations_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Novels: On Location', js: !ENV['GITHUB_ACTIONS'], type: :system do
+  if ENV['GITHUB_ACTIONS']
+    pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
+  else
+    pending
+    # Scenario: I add a pin
+    #   Given it's in write mode
+    #   When I double-click the map
+    #   Then it prompts me for book keywords
+    #   And asks me to confirm the first matching book
+    #   And adds a pin
+    #
+    # Scenario: I search for a specific place
+    #   When I enter a specific place
+    #   Then it prompts me for book keywords
+    #   And asks me to confirm the first matching book
+    #   And adds a pin
+    #
+    # Scenario: I tag a pin
+    #   Given an open balloon
+    #   When I click tag
+    #   Then it should prompt me for keywords
+    #   And add the tag
+    #
+    # Scenario: I delete a pin
+    #   Given an open balloon
+    #   When I click delete
+    #   Then it asks for confirmation
+    #   And deletes the pin
+  end
+end
--- spec/features/searching_locations_spec.rb
+++ spec/features/searching_locations_spec.rb
@@ -2,32 +2,75 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Novels: On Location', js: !ENV['GITHUB_ACTIONS'], type: :system do
+describe 'Browsing novel Locations', js: !ENV['GITHUB_ACTIONS'], type: :system do
   if ENV['GITHUB_ACTIONS']
     pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
   else
-    context 'when multiple Locations exist' do
-      let(:sun_also_rises) do
+    context 'when a Location exists and a User clicks its pin' do
+      before do
         Location.destroy_all
-        Location.create title: 'From Hell',
-                        lat_lng: ['51.519326', '-0.074316']
-        Location.create title: 'The Sun Also Rises',
+        Location.create author: 'Alan Moore',
+                        lat_lng: ['51.519326', '-0.074316'],
+                        title: 'From Hell'
+        visit root_path
+        find('div[role=button]').click
+      end
+
+      it('the map opens its balloon') { expect(page).to have_css 'div.map-balloon' }
+      it('the balloon shows a Zoom button') { expect(page).to have_css 'input[value=Zoom]' }
+      it('the balloon shows a Tag button') { expect(page).to have_css 'input[value=Tag]' }
+      it('the balloon shows a Delete button') { expect(page).to have_css 'input[value=Delete]' }
+
+      context 'when a User clicks Zoom' do
+        before { click_link_or_button 'Zoom' }
+
+        it('the map zooms in') { expect(evaluate_script('nOL.map.zoom')).to be > 10 }
+      end
+    end
+
+    context 'when multiple Locations exist' do
+      subject(:visible_pins) { evaluate_script script }
+
+      let(:script) do
+        'Object.values(nOL.pins).filter((pin) => pin.map).length;' # count visible pins (where map is defined)
+      end
+
+      before do
+        Location.destroy_all
+        Location.create author: 'Alan Moore',
+                        lat_lng: ['51.519326', '-0.074316'],
+                        tags: 'The Ten Bells, Spitalfields',
+                        title: 'From Hell'
+        Location.create author: 'Ernest Hemingway',
+                        title: 'The Sun Also Rises',
                         lat_lng: ['42.817422', '-1.64325']
       end
 
-      context 'when User searches for a specific Location' do
-        subject(:visible_pins) do
-          # count visible pins (where map is defined)
-          script = 'Object.values(nOL.pins).filter((pin) => pin.map).length;'
-          evaluate_script script
-        end
-
+      context 'when User searches for a matching author' do
         before do
           visit root_path
-          fill_in 'book-input', with: sun_also_rises.title
+          fill_in 'book-input', with: 'hemingway'
         end
 
-        it('hides other Locations') { expect(visible_pins).to eq 1 }
+        it('the map hides other Locations') { expect(visible_pins).to eq 1 }
+      end
+
+      context 'when User searches for a matching title' do
+        before do
+          visit root_path
+          fill_in 'book-input', with: 'sun also rises'
+        end
+
+        it('the map hides other Locations') { expect(visible_pins).to eq 1 }
+      end
+
+      context 'when User searches for a matching tag' do
+        before do
+          visit root_path
+          fill_in 'book-input', with: 'ten bells'
+        end
+
+        it('the map hides other Locations') { expect(visible_pins).to eq 1 }
       end
     end
   end
