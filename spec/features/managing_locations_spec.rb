c601e2c65429390fbb7ad3d1b750e79b52b9a919
Roderick Monje <rod@foveacentral.com>
2024-11-19 15:58:10 -0800

    spec: tag Location

--- spec/features/managing_locations_spec.rb
+++ spec/features/managing_locations_spec.rb
@@ -2,11 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Novels: On Location', js: !ENV['GITHUB_ACTIONS'], type: :system do
+RSpec.describe 'Managing Locations', js: !ENV['GITHUB_ACTIONS'], type: :system do
   if ENV['GITHUB_ACTIONS']
     pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
   else
-    pending
     # Scenario: I add a pin
     #   Given it's in write mode
     #   When I double-click the map
@@ -19,13 +18,24 @@ RSpec.describe 'Novels: On Location', js: !ENV['GITHUB_ACTIONS'], type: :system 
     #   Then it prompts me for book keywords
     #   And asks me to confirm the first matching book
     #   And adds a pin
-    #
-    # Scenario: I tag a pin
-    #   Given an open balloon
-    #   When I click tag
-    #   Then it should prompt me for keywords
-    #   And add the tag
-    #
+
+    context 'when a Location exists, its balloon is open, and a User tags it' do
+      before do
+        Location.destroy_all
+        Location.create author: 'Alan Moore',
+                        lat_lng: ['51.519326', '-0.074316'],
+                        title: 'From Hell'
+        visit root_path
+        find('div[role=button]').click
+        click_link_or_button 'Tag'
+        accept_prompt(with: 'The Ten Bells')
+      end
+
+      # rubocop:disable RSpec/NoExpectationExample
+      it("the Location's tag persists") { wait_for { Location.first.reload.tags }.to eq 'The Ten Bells' }
+      # rubocop:enable RSpec/NoExpectationExample
+    end
+
     # Scenario: I delete a pin
     #   Given an open balloon
     #   When I click delete
