b3cc82f2efd7218d93e96d71ba1e3ed7d02e2fce
Roderick Monje <rod@foveacentral.com>
2024-11-18 22:23:58 -0800

    fix

--- spec/features/searching_locations_spec.rb
+++ spec/features/searching_locations_spec.rb
@@ -6,8 +6,7 @@ require 'rails_helper'
 RSpec.describe 'Searching for Locations', js: !ENV['GITHUB_ACTIONS'], type: :system do
   scenario 'User searches for a Location' do
     if ENV['GITHUB_ACTIONS']
-      pending 'Disabling JavaScript on CI until Selenium bug is fixed'
-      # https://github.com/SeleniumHQ/selenium/issues/14609
+      pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
       raise
     else
       Location.destroy_all
