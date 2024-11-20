aa252d80a1d92130c05af77634627729bb81f816
Roderick Monje <rod@foveacentral.com>
2024-11-20 15:47:35 -0800

    spec: reader's link to facebook

--- spec/features/browsing_locations_spec.rb
+++ spec/features/browsing_locations_spec.rb
@@ -49,6 +49,11 @@ describe 'Browsing novel Locations', js: !ENV['GITHUB_ACTIONS'], type: :system d
       it('the balloon shows a Remap button') { expect(page).to have_button 'Remap' }
       it('the balloon shows a Tag button') { expect(page).to have_button 'Tag' }
       it('the balloon shows a Delete button') { expect(page).to have_button 'Delete' }
+
+      it "the balloon shows the novel's reader linked to Facebook" do
+        expect(page).to have_link 'Roderick Monje', href: 'http://www.facebook.com/profile.php?id=666325406'
+      end
+
       it('the balloon shows an All Locations for Novel button') { expect(page).to have_link 'All Locations for Novel' }
       it('the balloon shows an All Novels by Author button') { expect(page).to have_link 'All Novels by Author' }
       it('the balloon shows an All Pins by Reader button') { expect(page).to have_link 'All Pins by Reader' }
