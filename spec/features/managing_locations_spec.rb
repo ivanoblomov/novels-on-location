1cdeb3b2a404217060ed25c60c4248a882224c4c
Roderick Monje <ivanoblomov@users.noreply.github.com>
2024-11-19 20:05:32 -0800

    spec: managing Locations (#32)
    # Re: #27
    
    ## Goal
    Cover creating, tagging, and deleting Locations.
    
    ## Approach
    1. Reuse logic for opening a pin's balloon from
    `browsing_locations_spec.rb`.
    2. Use `accept_prompt(with:)` and `accept_confirm` to handle JavaScript
    prompts.
    3. Handle race conditions with:
      a. `wait_for` expectations
      b. block form of `accept_prompt`
    5. Add `pending 'Amazon integration'` for functionality that requires
    Amazon's Product Advertising API.
    6. Call `nOL.promptForBook` manually since double-clicking
    programmatically (with both Capybara *and* JS!) fails.
    
    Signed-off-by: Roderick Monje <rod@foveacentral.com>

---
+++ spec/features/managing_locations_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Managing Locations', js: !ENV['GITHUB_ACTIONS'], type: :system do
+  if ENV['GITHUB_ACTIONS']
+    pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
+  else
+    context 'when the map is in write mode and a User double-clicks it' do
+      before do
+        Location.destroy_all
+        visit root_path
+        find_by_id('mode-button').click
+        execute_script 'nOL.promptForBook(new google.maps.LatLng(51.519326, -.074316))' # HACK: since double-click fails
+        accept_confirm(with: 'from hell')
+        pending 'Amazon integration'
+      end
+
+      it('the Location is created') { expect(Location.count).to eq 1 }
+    end
+
+    context 'when a User enters a specific place and keywords for the book' do
+      before do
+        Location.destroy_all
+        visit root_path
+        fill_in 'place-input', with: "san sebastian\n"
+        accept_prompt(with: 'sun also rises')
+        accept_alert
+        pending 'Amazon integration'
+      end
+
+      it('the Location is created') { expect(Location.count).to eq 1 }
+    end
+
+    context 'when a Location exists and its balloon is open' do
+      before do
+        Location.destroy_all
+        Location.create author: 'Alan Moore',
+                        lat_lng: ['51.519326', '-0.074316'],
+                        title: 'From Hell'
+        visit root_path
+        find('div[role=button]').click
+      end
+
+      context 'when a User tags it' do
+        before { accept_prompt(with: 'The Ten Bells') { click_link_or_button 'Tag' } }
+
+        # rubocop:disable RSpec/NoExpectationExample
+        it("the Location's tag persists") { wait_for { Location.first.reload.tags }.to eq 'The Ten Bells' }
+        # rubocop:enable RSpec/NoExpectationExample
+      end
+
+      context 'when a User deletes it' do
+        before do
+          click_link_or_button 'Delete'
+          accept_confirm
+        end
+
+        # rubocop:disable RSpec/NoExpectationExample
+        it('the Location no longer exists') { wait_for { Location.count }.to eq 0 }
+        # rubocop:enable RSpec/NoExpectationExample
+      end
+    end
+  end
+end
