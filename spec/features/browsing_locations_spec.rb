96f925ed1cee7a702f9579edd88f5c171f8da228
Roderick Monje <rod@foveacentral.com>
2024-11-20 21:06:20 -0800

    spec: really fix race

--- spec/features/browsing_locations_spec.rb
+++ spec/features/browsing_locations_spec.rb
@@ -35,8 +35,11 @@ describe 'Browsing novel Locations', js: !ENV['GITHUB_ACTIONS'], type: :system d
         find('div[role=button]').click
       end
 
-      it('the map opens its balloon') { expect(page).to have_css 'div.map-balloon' }
-      it('the balloon doesn\'t show "null"') { expect(page).not_to have_css "h2", text: 'null' }
+      # rubocop:disable RSpec/MultipleExpectations
+      it 'the map opens its balloon' do
+        expect(page).to have_css 'div.map-balloon' and expect(page).to have_no_css 'h2', text: 'null'
+      end
+      # rubocop:enable RSpec/MultipleExpectations
 
       it "the balloon shows the novel's title linked to Amazon" do
         expect(page).to have_link 'From Hell', href: from_hell.amazon_url
