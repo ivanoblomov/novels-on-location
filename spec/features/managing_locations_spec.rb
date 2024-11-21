df1c0d318017078a1083dd485b409c492a8fb10b
Roderick Monje <rod@foveacentral.com>
2024-11-20 16:19:41 -0800

    fix: race

--- spec/features/managing_locations_spec.rb
+++ spec/features/managing_locations_spec.rb
@@ -22,9 +22,10 @@ RSpec.describe 'Managing Locations', js: !ENV['GITHUB_ACTIONS'], type: :system d
     context 'when a User enters a specific place and keywords for the book' do
       before do
         Location.destroy_all
-        visit root_path
-        fill_in 'place-input', with: "san sebastian\n"
-        accept_prompt(with: 'sun also rises')
+        accept_prompt(with: 'sun also rises') do
+          visit root_path
+          fill_in 'place-input', with: "san sebastian\n"
+        end
         accept_alert
         pending 'Amazon integration'
       end
