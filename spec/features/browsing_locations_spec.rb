635456e637b5a7b996ff6d3ee36cf10e5c8f493c
Roderick Monje <rod@foveacentral.com>
2024-11-20 20:52:27 -0800

    Revert "fix: race"
    This reverts commit 869db2c3875aff73ff7d4c94b4c7c5ba085c0b3c.

--- spec/features/browsing_locations_spec.rb
+++ spec/features/browsing_locations_spec.rb
@@ -32,7 +32,7 @@ describe 'Browsing novel Locations', js: !ENV['GITHUB_ACTIONS'], type: :system d
         Location.destroy_all
         from_hell
         visit root_path
-        wait_for { find('div[role=button]').click }
+        find('div[role=button]').click
       end
 
       it('the map opens its balloon') { expect(page).to have_css 'div.map-balloon' }
