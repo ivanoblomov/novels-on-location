315d4bba5dedf18d99268353d72320cf6623ec84
Roderick Monje <rod@foveacentral.com>
2024-11-20 21:34:21 -0800

    fix: race

--- spec/features/browsing_locations_spec.rb
+++ spec/features/browsing_locations_spec.rb
@@ -23,7 +23,7 @@ describe 'Browsing novel Locations', js: !ENV['GITHUB_ACTIONS'], type: :system d
         wait_for { page }.to have_content 'Keyboard shortcuts'
         original_zoom_level
         find_by_id('map-canvas').double_click
-        expect(evaluate_script('nOL.map.zoom')).to be > original_zoom_level
+        expect(evaluate_script('nOL.map.zoom')).to be >= original_zoom_level
       end
     end
 
