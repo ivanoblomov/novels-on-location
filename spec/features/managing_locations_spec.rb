286bc5e8ab1bded7cfa6540ae9bf0efc94b5513e
Roderick Monje <rod@foveacentral.com>
2024-11-24 12:09:36 -0800

    Revert "fix: selenium_headless"
    This reverts commit ca001b33cd86f8f544c86be142365bc9cb80e5ae.

--- spec/features/managing_locations_spec.rb
+++ spec/features/managing_locations_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Managing Locations', js: :selenium_headless, type: :system do
+RSpec.describe 'Managing Locations', js: :selenium_chrome_headless, type: :system do
     context 'when the map is in "Add Pins" mode and a User double-clicks it and enters keywords' do
       before do
         Location.destroy_all
