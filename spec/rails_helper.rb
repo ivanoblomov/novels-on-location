682645a2ca915847a31606eebf59842dce3a47c2
Roderick Monje <ivanoblomov@users.noreply.github.com>
2025-01-25 17:48:53 -0500

    conf: log js errors (#63)
    # Re: #62
    
    ## Goal
    Surface JS errors to help troubleshoot #62.
    
    ## Approach
    1. [Log JavaScript
    errors](https://stackoverflow.com/questions/49282691/how-to-display-js-errors-in-capybara-chromedriver)
    on [failure](https://bensnape.com/2014/08/01/rspec-after-failure-hook).
    2. (Fix deprecated meta tag: `apple-mobile-web-app-capable`.)
    
    Signed-off-by: Roderick Monje <rod@foveacentral.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,9 @@ RSpec.configure do |config|
   config.before(:each, :js, type: :system) do |example|
     driven_by example.metadata[:js]
   end
+  config.after(:each, type: :system) do |example|
+    warn page.driver.browser.logs.get(:browser) if example.exception
+  end
 end
 Capybara.configure do |config|
   config.default_driver = :selenium
