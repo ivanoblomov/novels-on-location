be631aa9c8537d41ed88d1d7cd55c472e70b7e63
Roderick Monje <rod@foveacentral.com>
2026-04-10 18:05:21 -0400

    conf: disable allow

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,7 +77,7 @@ Capybara.register_driver :selenium do |app|
                                  options: options)
 end
 VCR.configure do |config|
-  config.allow_http_connections_when_no_cassette = true
+  config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
