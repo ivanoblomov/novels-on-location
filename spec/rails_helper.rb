ae991612bd75f6866172271048144ccd33a425f8
Roderick Monje <rod@foveacentral.com>
2026-04-10 16:32:00 -0400

    conf: allow connections when no cassette exists

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,6 +77,7 @@ Capybara.register_driver :selenium do |app|
                                  options: options)
 end
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
