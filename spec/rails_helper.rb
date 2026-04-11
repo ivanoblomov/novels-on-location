ecd218dc98f1e41a77eb612276059a3dafa12433
Roderick Monje <rod@foveacentral.com>
2026-04-10 22:09:06 -0400

    spec: abc

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,8 +79,8 @@ end
 VCR.configure do |config|
   config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = 'spec/vcr_cassettes'
-  config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.hook_into :webmock
 
   # Crucial: Don't leak your keys in the cassettes!
   config.filter_sensitive_data('<FACEBOOK_APP_ID>') { ENV.fetch('FACEBOOK_APP_ID', nil) }
