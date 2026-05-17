7331fd5e23162b06263c1c9ea9d4dad82017c90f
Roderick Monje <rod@foveacentral.com>
2026-05-17 14:37:53 -0700

    Revert "conf: bumped gems (#103)"
    This reverts commit 447510333302c5fbcf4023ed749f7727b59bb0f1.

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -27,7 +27,7 @@ module Admin
     private
 
     def find_location
-      @location = Location.find params.expect(:id)
+      @location = Location.find params[:id]
     end
 
     def find_locations
