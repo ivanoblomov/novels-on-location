2ec86d0bbc6853be6be91823c1dcfed7eaf12faa
Roderick Monje <rod@foveacentral.com>
2026-05-18 13:09:55 -0700

    conf: javascript_driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,12 +66,17 @@ RSpec.configure do |config|
 end
 Capybara.configure do |config|
   config.default_driver = :selenium
-  config.javascript_driver = :selenium
+  config.javascript_driver = :selenium_chrome_headless
   config.app_host = 'http://localhost'
 end
 Capybara.always_include_port = true
-Capybara.register_driver :selenium do |app|
+Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--disable-dev-shm-usage') # Forces Chrome to use disk instead of /dev/shm memory
+  options.add_argument('--disable-gpu')
+  options.add_argument('--headless=new') # Use the modern headless engine
+  options.add_argument('--no-sandbox') # Essential for Linux containers
+  options.add_argument('--window-size=1400,1400')
   Capybara::Selenium::Driver.new(app,
                                  browser: :chrome,
                                  options: options)
