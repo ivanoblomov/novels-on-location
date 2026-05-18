1d40d1a720be708a767ed299f363bbbf3274e91b
Roderick Monje <rod@foveacentral.com>
2026-05-17 23:15:50 -0700

    refactor: rename ENV

--- spec/features/browsing_locations_spec.rb
+++ spec/features/browsing_locations_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe 'Browsing novel Locations', js: ENV['DRIVER'] ? ENV['DRIVER'].to_sym : :selenium_chrome_headless,
                                      type: :system do
-  if ENV['GITHUB_ACTIONSx']
+  if ENV['NOT_GITHUB_ACTIONS']
     pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
   else
     let(:from_hell) do
--- spec/features/managing_locations_spec.rb
+++ spec/features/managing_locations_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Managing Locations', js: ENV['DRIVER'] ? ENV['DRIVER'].to_sym : :selenium_chrome_headless,
                                      type: :system do
-  if ENV['GITHUB_ACTIONSx']
+  if ENV['NOT_GITHUB_ACTIONS']
     pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
   else
     context 'when the map is in "Add Pins" mode and a User double-clicks it and enters keywords' do
