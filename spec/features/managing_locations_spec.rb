2e5d1523e7e86210d9d124b19de86beccda361e3
Roderick Monje <ivanoblomov@users.noreply.github.com>
2026-05-18 23:15:42 -0700

    fix: unexpected token ',' (#114)
    # Closes: #45, #62
    
    ## Goal
    Fix features on CI.
    
    ## Approach
    1. Enable the feature specs by removing the conditionals on
    `ENV['GITHUB_ACTIONS']`.
    2. Add logging to a failing spec to surface the error:
        ```ruby
        p "facebook_app_id: #{Rails.application.config.facebook_app_id}"
        p '=== LINE-BY-LINE PAGE SOURCE ==='
        page.source.each_line.with_index(1) do |line, index|
          p "#{index}: #{line}" if index.between?(75, 90)
        end
        p '================================'
        ```
    3. The logs indicated that the empty string for `facebook_app_id`:
        ```
        fbAppId: <%= Rails.application.config.facebook_app_id %>,
        ```
        caused the JSON arguments to be malformed:
        ```ruby
        "=== LINE-BY-LINE PAGE SOURCE ==="
    "75: <script
    src=\"/assets/application-88da78049bfea8deb862f7ee6c0b85dcf93754f4586a7622c7c69ce3cfa487ac.js\"></script>\n"
        "76: <script>\n"
        "77:   window.fbAsyncInit=function(){\n"
        "78:     nOL.init({\n"
    "79: defaultTitle: \"Novels: On Location - 0 Novels/0 Locations\",\n"
        "80:       \n"
        "81:       fbAppId: ,\n"
        "82:       host: 'localhost',\n"
        "83:       selectedLocationSlug: null,\n"
        "84:     });\n"
        "85:   };\n"
        "86:   (function(d){\n"
        "87:     var js, id = 'facebook-jssdk';\n"
        "88:     if (d.getElementById(id))\n"
        "89:       return;\n"
        "90:     js = d.createElement('script');\n"
        "================================"
        ```
    4. Quote the interpolated variable so even a nil value won't cause a
    syntax error.
    5. Disable cop `RSpec/NestedGroups`.
    
    Signed-off-by: Roderick Monje <rod@foveacentral.com>

--- spec/features/managing_locations_spec.rb
+++ spec/features/managing_locations_spec.rb
@@ -4,90 +4,86 @@ require 'rails_helper'
 
 RSpec.describe 'Managing Locations', js: ENV['DRIVER'] ? ENV['DRIVER'].to_sym : :selenium_chrome_headless,
                                      type: :system do
-  if ENV['GITHUB_ACTIONS']
-    pending 'Disabling JavaScript on CI until Selenium bug is fixed: https://github.com/SeleniumHQ/selenium/issues/14609'
-  else
-    context 'when the map is in "Add Pins" mode and a User double-clicks it and enters keywords' do
+  context 'when the map is in "Add Pins" mode and a User double-clicks it and enters keywords' do
+    before do
+      visit root_path
+      Location.destroy_all
+      click_on 'Mode: Zoom'
+      execute_script 'nOL.promptForBook(new google.maps.LatLng(51.519326, -.074316))' # HACK: since double-click fails
+      accept_prompt with: 'from hell alan moore'
+      accept_confirm
+    end
+
+    # rubocop:disable RSpec/NoExpectationExample
+    it('the Location is created') { wait_for { Location.count }.to eq 1 }
+    # rubocop:enable RSpec/NoExpectationExample
+  end
+
+  context 'when a User enters a specific place, keywords for the book, and some notes' do
+    before do
+      Location.destroy_all
+      visit root_path
+      fill_in 'place-input', with: "san sebastian\n"
+      accept_prompt(with: 'sun also rises')
+      accept_confirm
+      accept_prompt(with: 'a swim after rejection')
+    end
+
+    it('the Location is created') { expect(Location.count).to eq 1 }
+  end
+
+  context 'when a Location exists and its balloon is open' do
+    before do
+      Location.destroy_all
+      Location.find_or_create_by author: 'Alan Moore',
+                                 lat_lng: ['51.519326', '-0.074316'],
+                                 title: 'From Hell'
+      visit root_path
+      find('div[role=button]').click
+    end
+
+    context 'when a User annotates it' do
+      before { accept_prompt(with: 'Originally serialized in Taboo') { click_link_or_button 'Annotate' } }
+
+      # rubocop:disable RSpec/NoExpectationExample
+      it("the Location's note persists") {
+        wait_for { Location.first.reload.notes }.to eq 'Originally serialized in Taboo'
+      }
+      # rubocop:enable RSpec/NoExpectationExample
+    end
+
+    context 'when a User remaps it' do
       before do
-        visit root_path
-        Location.destroy_all
-        click_on 'Mode: Zoom'
-        execute_script 'nOL.promptForBook(new google.maps.LatLng(51.519326, -.074316))' # HACK: since double-click fails
-        accept_prompt with: 'from hell alan moore'
+        original_coordinates
+        accept_prompt(with: '1 Highbury Place, Islington') { click_link_or_button 'Remap' }
+      end
+
+      let(:original_coordinates) { Location.first.lat_lng }
+
+      # rubocop:disable RSpec/NoExpectationExample
+      it("the Location's new coordinates persist") do
+        wait_for { Location.first.reload.lat_lng }.not_to eq original_coordinates
+      end
+      # rubocop:enable RSpec/NoExpectationExample
+    end
+
+    context 'when a User tags it' do
+      before { accept_prompt(with: 'The Ten Bells') { click_link_or_button 'Tag' } }
+
+      # rubocop:disable RSpec/NoExpectationExample
+      it("the Location's tag persists") { wait_for { Location.first.reload.tags }.to eq 'The Ten Bells' }
+      # rubocop:enable RSpec/NoExpectationExample
+    end
+
+    context 'when a User deletes it' do
+      before do
+        click_link_or_button 'Delete'
         accept_confirm
       end
 
       # rubocop:disable RSpec/NoExpectationExample
-      it('the Location is created') { wait_for { Location.count }.to eq 1 }
+      it('the Location no longer exists') { wait_for { Location.count }.to eq 0 }
       # rubocop:enable RSpec/NoExpectationExample
-    end
-
-    context 'when a User enters a specific place, keywords for the book, and some notes' do
-      before do
-        Location.destroy_all
-        visit root_path
-        fill_in 'place-input', with: "san sebastian\n"
-        accept_prompt(with: 'sun also rises')
-        accept_confirm
-        accept_prompt(with: 'a swim after rejection')
-      end
-
-      it('the Location is created') { expect(Location.count).to eq 1 }
-    end
-
-    context 'when a Location exists and its balloon is open' do
-      before do
-        Location.destroy_all
-        Location.find_or_create_by author: 'Alan Moore',
-                                   lat_lng: ['51.519326', '-0.074316'],
-                                   title: 'From Hell'
-        visit root_path
-        find('div[role=button]').click
-      end
-
-      context 'when a User annotates it' do
-        before { accept_prompt(with: 'Originally serialized in Taboo') { click_link_or_button 'Annotate' } }
-
-        # rubocop:disable RSpec/NoExpectationExample
-        it("the Location's note persists") {
-          wait_for { Location.first.reload.notes }.to eq 'Originally serialized in Taboo'
-        }
-        # rubocop:enable RSpec/NoExpectationExample
-      end
-
-      context 'when a User remaps it' do
-        before do
-          original_coordinates
-          accept_prompt(with: '1 Highbury Place, Islington') { click_link_or_button 'Remap' }
-        end
-
-        let(:original_coordinates) { Location.first.lat_lng }
-
-        # rubocop:disable RSpec/NoExpectationExample
-        it("the Location's new coordinates persist") do
-          wait_for { Location.first.reload.lat_lng }.not_to eq original_coordinates
-        end
-        # rubocop:enable RSpec/NoExpectationExample
-      end
-
-      context 'when a User tags it' do
-        before { accept_prompt(with: 'The Ten Bells') { click_link_or_button 'Tag' } }
-
-        # rubocop:disable RSpec/NoExpectationExample
-        it("the Location's tag persists") { wait_for { Location.first.reload.tags }.to eq 'The Ten Bells' }
-        # rubocop:enable RSpec/NoExpectationExample
-      end
-
-      context 'when a User deletes it' do
-        before do
-          click_link_or_button 'Delete'
-          accept_confirm
-        end
-
-        # rubocop:disable RSpec/NoExpectationExample
-        it('the Location no longer exists') { wait_for { Location.count }.to eq 0 }
-        # rubocop:enable RSpec/NoExpectationExample
-      end
     end
   end
 end
