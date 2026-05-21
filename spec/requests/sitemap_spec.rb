48506968acd372755adddf5ce56f37ff144536a0
Roderick Monje <rod@foveacentral.com>
2026-05-21 14:10:06 -0700

    spec: integration_path

--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -3,6 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'routes' do
+  describe '/integration' do
+    it do
+      get integration_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
   describe '/sitemap.xml' do
     it do
       get sitemap_path, headers: { 'ACCEPT' => 'application/xml' }
