44f638b3a7a729f1285f81a7b3b445188bbe3b7c
Roderick Monje <rod@foveacentral.com>
2026-05-21 14:00:09 -0700

    spec: formatted

--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'routes' do
-  describe 'GET /sitemap.xml' do
-    it 'returns a successful response' do
+  describe '/sitemap.xml' do
+    it do
       get sitemap_path, headers: { 'ACCEPT' => 'application/xml' }
       expect(response).to have_http_status(:ok)
     end
