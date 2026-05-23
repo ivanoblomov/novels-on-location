98bb3d96de258332f6c7d7d8cd0e1972c6c4bf0f
Roderick Monje <rod@foveacentral.com>
2026-05-22 17:00:49 -0700

    refactor: main_host

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@
 require File.expand_path('application', __dir__)
 NovelsOnLocation::Application.initialize!
 NovelsOnLocation::Application.configure do
-  config.admin_email = 'ed@novelsonlocation.com'
+  config.admin_email = "ed@#{Rails.application.config.main_host}"
   config.facebook_app_id = ENV.fetch('FACEBOOK_APP_ID', nil)
 end
