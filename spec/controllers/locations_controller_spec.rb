3cd86a960ca2dc69d21ee5884d6c16b40a685283
Roderick Monje <rod@foveacentral.com>
2026-05-24 14:12:54 -0700

    spec: LocationsController#bookmark; DRYd with before

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -6,9 +6,17 @@ describe LocationsController do
   let(:location) { instance_spy(Location) }
   let(:user) { instance_double(User, id: 1) }
 
-  it 'DELETE unbookmark' do
+  before do
     allow(User).to receive(:new).and_return(user)
     allow(Location).to receive(:find).and_return(location)
+  end
+
+  it 'PUT bookmark' do
+    put :bookmark, format: :json, params: { id: 1 }
+    expect(location).to have_received(:add_bookmark).with(user.id)
+  end
+
+  it 'DELETE unbookmark' do
     delete :unbookmark, format: :json, params: { id: 1 }
     expect(location).to have_received(:remove_bookmark).with(user.id)
   end
