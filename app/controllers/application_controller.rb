a8b0b43d0abad0df8c2b24d9ecdc883fbcd53936
Roderick Monje <rod@foveacentral.com>
2026-05-25 22:11:32 -0700

    refactor: unless

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,10 @@ class ApplicationController < ActionController::Base
                        with: :null_session
   rescue_from Mongoid::Errors::DocumentNotFound,
               with: :error404
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception do |exception|
-      @exception = exception
-      NOT_FOUND.match?(@exception.message) ? error404 : error500
-    end
-  end
+  rescue_from Exception do |exception|
+    @exception = exception
+    NOT_FOUND.match?(@exception.message) ? error404 : error500
+  end unless Rails.application.config.consider_all_requests_local
 
   def authenticate_user!
     redirect_to root_url unless current_user.admin? || Rails.env.development?
