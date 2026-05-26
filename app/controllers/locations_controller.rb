ad87eb56fe8ac8d66b96a2d29947cafa0ef1d32d
Roderick Monje <rod@foveacentral.com>
2026-05-25 18:32:40 -0700

    refactor?: removed #canonical_location_url, #find_location_by_id

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -71,24 +71,10 @@ class LocationsController < ApplicationController
 
   private
 
-  def canonical_location_url
-    location_url @location, canonical_url: location_url(@location)
-  end
-
   def find_location
-    @location = find_location_by_id ||
-                Location.find(Moped::BSON::ObjectId(params.expect(:id)))
-  rescue StandardError
-    if Location.exists?(id: params[:id])
-      @location = find_location_by_id
-      redirect_to canonical_location_url, status: :moved_permanently
-    else
-      error404
-    end
-  end
+    return if (@location = Location.find(params.expect(:id)))
 
-  def find_location_by_id
-    Location.find params.expect(:id)
+    error404
   end
 
   def format_response
