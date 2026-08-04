4dd99e4bbaf4d25a4898add569ef4c191b505e20
Roderick Monje <rod@foveacentral.com>
2026-08-04 14:06:56 -0700

    Revert "refactor: qlty check --fix"
    This reverts commit ee6bf15de2879313765137fedb478fd198bf8bb5.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1020,7 +1020,7 @@ jQuery.cookie = function (name, value, options) {
 			for (var i = 0; i < cookies.length; i++) {
 				var cookie = jQuery.trim(cookies[i]);
 				// Does this cookie string begin with the name we want?
-				if (cookie.substring(0, name.length + 1) === name + "=") {
+				if (cookie.substring(0, name.length + 1) == name + "=") {
 					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
 					break;
 				}
