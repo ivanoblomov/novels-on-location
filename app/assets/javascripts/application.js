50feec2946ef91a149fe87204e870ae709e9dc05
Roderick Monje <rod@foveacentral.com>
2026-08-04 15:58:09 -0700

    fix: Template literals are preferred over string concatenation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1017,7 +1017,7 @@ jQuery.cookie = function (name, value, options) {
 			for (let i = 0; i < cookies.length; i++) {
 				const cookie = jQuery.trim(cookies[i]);
 				// Does this cookie string begin with the name we want?
-				if (cookie.substring(0, name.length + 1) === name + "=") {
+				if (cookie.substring(0, name.length + 1) === `${name}=`) {
 					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
 					break;
 				}
