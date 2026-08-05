1551e43292f842a34254b2dd6025640c68980b6f
Roderick Monje <rod@foveacentral.com>
2026-08-05 16:22:15 -0700

    fix: failures

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.config.dartsass.builds = {
-  "admin.scss" => "admin.css"
+  '1140.css.scss' => 'admin.css',
+  'admin.css.scss' => 'admin.css',
+  'default.css.scss' => 'admin.css',
+  'ie.css.scss' => 'admin.css',
+  'ios.css.scss' => 'admin.css',
+  'print.css.scss' => 'admin.css',
+  'safari.css.scss' => 'admin.css'
 }
