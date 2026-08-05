12207cfa60208be8e84a146a8e490be5739a3d3c
Roderick Monje <rod@foveacentral.com>
2026-08-05 16:43:42 -0700

    fix: DRYd dartsass config

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.dartsass.builds = {
-  '1140.css.scss' => 'admin.css',
-  'admin.css.scss' => 'admin.css',
-  'default.css.scss' => 'admin.css',
-  'ie.css.scss' => 'admin.css',
-  'ios.css.scss' => 'admin.css',
-  'print.css.scss' => 'admin.css',
-  'safari.css.scss' => 'admin.css'
+  '.' => '.'
 }
