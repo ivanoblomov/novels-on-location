aa64ed31dfd1b4e29fdce4b167c4f03a121523d9
Roderick Monje <rod@foveacentral.com>
2026-08-06 12:28:47 -0700

    conf: match test's caching settings to prod

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,6 +15,6 @@ NovelsOnLocation::Application.configure do
   config.main_host = 'localhost'
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => 'public, max-age=31536000, immutable'
   }
 end
