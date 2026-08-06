fb80682d568798ed1922f58f720828108e14b123
Roderick Monje <rod@foveacentral.com>
2026-08-06 12:50:44 -0700

    conf: remove deprecated sprockets settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,10 +6,6 @@ NovelsOnLocation::Application.configure do
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = 'X-Sendfile'
   config.active_support.deprecation = :notify
-  config.assets.compile = false
-  config.assets.compress = true
-  config.assets.digest = true
-  config.assets.precompile += %w[*.css *.js *.js.erb]
   config.cache_classes = true
   config.consider_all_requests_local = false
   config.eager_load = true
