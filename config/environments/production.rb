9eba455477d56c291b0e99c1527bb5d2f88c178e
Roderick Monje <ivanoblomov@users.noreply.github.com>
2026-08-06 16:35:44 -0700

    UX: boost Lighthouse scores (#201)
    # Re: #141
    
    ## Goal
    Boost Lighthouse scores.
    
    ## Approach
    1. `preconnect` recommended origins.
    2. Configure `puma-dev` to run in production (optionally) with
    `.powenv`.
    3. Tweak `production.rb` to:
        1. enable SSL
        2. use efficient cache lifetimes
        3. remove outdated sprockets settings
    4. Match `test.rb` to 3ii.
    5. `defer` or `preload` JS or CSS, respectively. (This was the biggest
    win.)
    6. Trap nil `jQuery`.
    
    This boosted the scores to the theoretical maxixum (since all remaining
    issues are for third-party code):
    
    <img width="760" height="276" alt="Screenshot 2026-08-06 at 1 35 54 PM"
    src="https://github.com/user-attachments/assets/4c3f0499-9594-40f7-a1bd-413475e41a66"
    />
    
    Signed-off-by: Roderick Monje <rod@foveacentral.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,20 +2,21 @@
 
 ENV['RAILS_ASSET_ID'] = ''
 NovelsOnLocation::Application.configure do
+  config.assume_ssl = true
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = 'X-Sendfile'
   config.active_support.deprecation = :notify
-  config.assets.compile = false
-  config.assets.compress = true
-  config.assets.digest = true
-  config.assets.precompile += %w[*.css *.js *.js.erb]
   config.cache_classes = true
   config.consider_all_requests_local = false
   config.eager_load = true
   config.i18n.fallbacks = true
   config.log_level = :info
   config.serve_static_files = true
-  config.static_cache_control = 'public, max-age=31556926'
+  config.static_cache_control = 'public, max-age=31536000, immutable'
   # custom
   config.main_host = 'novelsonlocation.com'
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000, immutable'
+  }
 end
