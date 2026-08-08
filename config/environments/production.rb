0ec79b47b0d08301a0d9b7da1699e64e5a106d91
Roderick Monje <rod@foveacentral.com>
2026-08-07 21:30:05 -0700

    Revert "refactor: remove obsolete settings"
    This reverts commit 72406dfee2ca28c341277fefa4d25b2620e1a289.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,10 +6,13 @@ NovelsOnLocation::Application.configure do
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = 'X-Sendfile'
   config.active_support.deprecation = :notify
+  config.cache_classes = true
   config.consider_all_requests_local = false
   config.eager_load = true
   config.i18n.fallbacks = true
   config.log_level = :info
+  config.serve_static_files = true
+  config.static_cache_control = 'public, max-age=31536000, immutable'
   # custom
   config.main_host = 'novelsonlocation.com'
   config.public_file_server.enabled = true
