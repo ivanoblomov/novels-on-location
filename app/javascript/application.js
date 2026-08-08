d30d53e42e065cb4f8652c1973085f8bbe808133
Roderick Monje <rod@foveacentral.com>
2026-08-08 14:50:14 -0700

    fix: qlty fmt

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -890,59 +890,59 @@ applesearch.clearBtnClick = function () {
  * @author Klaus Hartl/klaus.hartl@stilbuero.de
  */
 // qlty-ignore: qlty:function-complexity
-	// qlty-ignore: biome:lint/suspicious/noAssignInExpressions
-	nOL.cookie = (name, value, options) => {
-		if (typeof value !== "undefined") {
-			// name and value given, set cookie
-			options = options || {};
-			if (value === null) {
-				value = "";
-				options.expires = -1;
-			}
-			let expires = "";
-			if (
-				options.expires &&
-				(typeof options.expires === "number" || options.expires.toUTCString)
-			) {
-				let date;
-				if (typeof options.expires === "number") {
-					date = new Date();
-					date.setTime(date.getTime() + options.expires * 24 * 60 * 60 * 1000);
-				} else {
-					date = options.expires;
-				}
-				expires = `; expires=${date.toUTCString()}`; // use expires attribute, max-age is not supported by IE
-			}
-			// CAUTION: Needed to parenthesize options.path and options.domain
-			// in the following expressions, otherwise they evaluate to undefined
-			// in the packed version for some reason...
-			const path = options.path ? `; path=${options.path}` : "";
-			const domain = options.domain ? `; domain=${options.domain}` : "";
-			const secure = options.secure ? "; secure" : "";
-			// qlty-ignore: biome:lint/suspicious/noDocumentCookie
-			document.cookie = [
-				name,
-				"=",
-				encodeURIComponent(value),
-				expires,
-				path,
-				domain,
-				secure,
-			].join("");
-		} else {
-			// only name given, get cookie
-			let cookieValue = null;
-			if (document.cookie && document.cookie !== "") {
-				const cookies = document.cookie.split(";");
-				for (let i = 0; i < cookies.length; i++) {
-					const cookie = String.prototype.trim(cookies[i]);
-					// Does this cookie string begin with the name we want?
-					if (cookie.substring(0, name.length + 1) === `${name}=`) {
-						cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-						break;
-					}
-				}
-			}
-			return cookieValue;
+// qlty-ignore: biome:lint/suspicious/noAssignInExpressions
+nOL.cookie = (name, value, options) => {
+	if (typeof value !== "undefined") {
+		// name and value given, set cookie
+		options = options || {};
+		if (value === null) {
+			value = "";
+			options.expires = -1;
 		}
+		let expires = "";
+		if (
+			options.expires &&
+			(typeof options.expires === "number" || options.expires.toUTCString)
+		) {
+			let date;
+			if (typeof options.expires === "number") {
+				date = new Date();
+				date.setTime(date.getTime() + options.expires * 24 * 60 * 60 * 1000);
+			} else {
+				date = options.expires;
+			}
+			expires = `; expires=${date.toUTCString()}`; // use expires attribute, max-age is not supported by IE
+		}
+		// CAUTION: Needed to parenthesize options.path and options.domain
+		// in the following expressions, otherwise they evaluate to undefined
+		// in the packed version for some reason...
+		const path = options.path ? `; path=${options.path}` : "";
+		const domain = options.domain ? `; domain=${options.domain}` : "";
+		const secure = options.secure ? "; secure" : "";
+		// qlty-ignore: biome:lint/suspicious/noDocumentCookie
+		document.cookie = [
+			name,
+			"=",
+			encodeURIComponent(value),
+			expires,
+			path,
+			domain,
+			secure,
+		].join("");
+	} else {
+		// only name given, get cookie
+		let cookieValue = null;
+		if (document.cookie && document.cookie !== "") {
+			const cookies = document.cookie.split(";");
+			for (let i = 0; i < cookies.length; i++) {
+				const cookie = String.prototype.trim(cookies[i]);
+				// Does this cookie string begin with the name we want?
+				if (cookie.substring(0, name.length + 1) === `${name}=`) {
+					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
 	}
+};
